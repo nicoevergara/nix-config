19484dae2018a3750282007f3388adb8f0cb834b
Nico Vergara <me@nicoevergara.com>
2026-03-25 11:06:52 -0700

    temp

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -10,57 +10,62 @@
     mac-app-util.url = "github:hraban/mac-app-util";
   };
 
-  outputs = inputs @ {
-    self,
-    nix-darwin,
-    home-manager,
-    nixpkgs,
-    mac-app-util,
-  }: let
-    username = "nico.vergara";
-    system = "aarch64-darwin";
-    configuration = {pkgs, ...}: {
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      users.users.${username} = {
-        name = username;
-        home = "/Users/${username}";
-      };
-
-      system = {
-        configurationRevision = self.rev or self.dirtyRev or null;
-        stateVersion = 6;
-        primaryUser = username;
-      };
-
-      nixpkgs = {
-        config.allowUnfree = true;
-        hostPlatform = system;
-      };
-    };
-  in {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Nicos-MacBook-Pro
-    darwinConfigurations."Nicos-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [
-        configuration
-        mac-app-util.darwinModules.default
-        home-manager.darwinModules.home-manager
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      home-manager,
+      nixpkgs,
+      mac-app-util,
+    }:
+    let
+      username = "nico.vergara";
+      system = "aarch64-darwin";
+      configuration =
+        { pkgs, ... }:
         {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            sharedModules = [
-              mac-app-util.homeManagerModules.default
-            ];
-            extraSpecialArgs = {
-              inherit username;
-            };
-            users.${username} = import ./modules/home-manager/${username}/home.nix;
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          users.users.${username} = {
+            name = username;
+            home = "/Users/${username}";
           };
-        }
-      ];
+
+          system = {
+            configurationRevision = self.rev or self.dirtyRev or null;
+            stateVersion = 6;
+            primaryUser = username;
+          };
+
+          nixpkgs = {
+            config.allowUnfree = true;
+            hostPlatform = system;
+          };
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Nicos-MacBook-Pro
+      darwinConfigurations."Nicos-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          mac-app-util.darwinModules.default
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              sharedModules = [
+                mac-app-util.homeManagerModules.default
+              ];
+              extraSpecialArgs = {
+                inherit username;
+              };
+              users.${username} = import ./modules/home-manager/${username}/home.nix;
+            };
+          }
+        ];
+      };
     };
-  };
 }
